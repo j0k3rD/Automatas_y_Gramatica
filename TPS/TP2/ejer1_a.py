4f438f7de20f4ac7444849e11697cb358294108d
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-03-28 22:57:20 -0300

    ejer1_a tp2 finished

--- TPS/TP2/ejer1_a.py
+++ TPS/TP2/ejer1_a.py
@@ -1,4 +1,13 @@
-##
-sadasdasd
-asdasd
-asdasdasd
+# Ejercicio 1_a -- Automata para '[A|B]'
+
+def automata():
+    aut = []
+    c = str(input("Ingrese un caracter entre [A|B]: "))
+    while c == 'A' or c == 'B':
+        aut.append(c)
+        print("Exelente!")
+        print("La cadena hasta ahora es:", aut)
+        c = str(input("Ingrese otro caracter entre [A|B]: "))
+    print("Error. No es compatible con el Automata.")
+
+automata()
