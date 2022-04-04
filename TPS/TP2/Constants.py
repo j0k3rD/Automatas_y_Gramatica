15953d8fe84668d027f3892559ac476dd03ee314
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-04 18:24:13 -0300

    starting tp2

---
+++ TPS/TP2/Constants.py
@@ -0,0 +1,5 @@
+# Constantes para los Automatas
+
+INIT_STATE = "Comienza el Automata"
+NEXT_STATE = "Seguiente Estado: "
+RTN_STATE = "Vuelvo al Estado: "
--- TPS/TP2/ejer1_a.py
+++ TPS/TP2/ejer1_a.py
@@ -1,13 +1,41 @@
 # Ejercicio 1_a -- Automata para '[A|B]'
+import Constants
 
-def automata():
-    aut = []
-    c = str(input("Ingrese un caracter entre [A|B]: "))
-    while c == 'A' or c == 'B':
-        aut.append(c)
-        print("Exelente!")
-        print("La cadena hasta ahora es:", aut)
-        c = str(input("Ingrese otro caracter entre [A|B]: "))
-    print("Error. No es compatible con el Automata.")
 
-automata()
+def main():
+    lista = []
+    estado = 0
+    print(Constants.INIT_STATE)
+    create_state(estado)
+    x = print(input("Ingrese el Automata a evaluar: \n"))
+    lista.append(x)
+    change_state()
+    print(Constants.NEXT_STATE)
+    
+def switch_state(lista, estado):
+    for i in range(len(lista)):
+        if i == "a":
+            create_state(estado)
+        elif i == "b":
+            
+        elif i == "":
+            change_state()
+        else:
+            state_end()
+
+
+        
+def create_state(estado):
+    if switch_state():
+        estado +=1
+    
+
+
+def state_end(lista):
+    if lista.index(-1):
+        print("Estado finalizado")
+
+
+
+if __name__=='__main__':
+    main()
