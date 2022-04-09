8eaac7dc2c1440540ab0ed781eec575dc0756c8d
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-08 21:59:13 -0300

    ejer2 ended

--- TPS/TP2/ejer1_b.py
+++ TPS/TP2/ejer1_b.py
@@ -4,7 +4,7 @@ import sys
 from six.moves import input as raw_input
 
 def main():
-    transition = [[[0,1],[0]], [[4],[3]], [[4],[2]], [[4],[4]]]
+    transition = [[[0,1],[0]], [[0],[2]], [[2],[3]], [[4],[4]]]
     input = raw_input("Enter the string: ")
     input = list(input)
 
