38d03aad98d54c856ebee2f03c08c03cef02e67d
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-16 18:59:52 -0300

    TP3 ended

--- TPS/TP3/ejer_4.py
+++ TPS/TP3/ejer_4.py
@@ -1,12 +1,13 @@
-# Ejercicio 2 - TP3 - Automata: (a|b)*(a|b|e)
+# Ejercicio 4 (en clase) - TP3 - Automata: (a|b)*(a|bb)
 
 aut = input("Enter the string: ")
 aut.re
 
 dfa = {0:{'a':1, 'b':2, ' ':1},
        1:{'a':1, 'b':3, ' ':2}, 
-       2:{'a':3, 'b':2, ' ':3},
-       3:{'a':3, 'b':3, ' ':3}}
+       2:{'a':1, 'b':2, ' ':3},
+       3:{'a':1, 'b':4, ' ':4},
+       4:{'a':1, 'b':2, ' ':4}}
 
 # accepting = "2"
 initial = 0
@@ -17,4 +18,4 @@ def accepts(transitions,initial,accepting,s):
         state = transitions[state][c]
     return state in accepting
 
-print(accepts(dfa,initial,{1,2,3},aut))
+print(accepts(dfa,initial,{4},aut))
